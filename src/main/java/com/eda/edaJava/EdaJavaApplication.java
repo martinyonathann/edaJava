55bdea4f350f0032bb74ee480dfed52b8ed71fe6
martinyonathann <martin.yonathan305@gmail.com>
2021-04-08 10:19:43 +0700

    add maven project

---
+++ src/main/java/com/eda/edaJava/EdaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.eda.edaJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eda/edaJava/EdaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eda.edaJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
